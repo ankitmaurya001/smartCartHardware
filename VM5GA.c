ba4fbeb1655ac0c078c183c8f314c974f9270890
truthSeeker <ankitmaurya001@gmail.com>
2015-11-25 21:00:05 +0530

    VM5GA responding to arduino

--- VM5GA.c
+++ VM5GA.c
@@ -2,11 +2,13 @@
  * This is for reading the rfid tags from the VM5GA RFID reader.
  * VM5GA is a UHF reader.
  * VM5GA will be connected to serial port 2 of Arduino mega.
+ * I am going to write pretty bad code , because of time constraint. Will have to come back to clean up.
  */
 
 
 byte info[] = {0xBB , 0x00, 0x03, 0x00, 0x01, 0x02, 0x06, 0x7E};
-
+byte infoResponse[] = {0xBB, 0x01, 0x03, 0x00, 0x08, 0x02, 0x4D, 0x61, 0x67, 0x69, 0x63, 0x52, 0x66, 0xA7, 0X7E};
+String readString = "";
 
 void setup() {
   // initialize both serial ports:
@@ -14,9 +16,10 @@ void setup() {
   Serial2.begin(115200);
   pinMode(8, OUTPUT);   //connected to EN pin
  digitalWrite(8,HIGH);  //make EN pin high
- Serial.println("Writing info");
- Serial.write(info, sizeof(info));
+ //Serial.println("Writing info");
  Serial2.write(info, sizeof(info));
+
+ //Serial2.write(info, sizeof(info));
 
 }
 
@@ -28,15 +31,41 @@ void setup() {
 //BB 00 03 00 01 02 06 7E ---> in hex
 //187 000 003 000 001 002 006 126  ---> in decimal
 void loop(){
-	String stringOne;
-	//get the module info.
-//if(Serial.available()){
-//	stringOne = String(Serial.read(),HEX);
-//	Serial.println(stringOne);
-//}
-//	if(Serial.available()){
-//		Serial.println(Serial.read());
-//		stringOne = String(Serial2.read(),HEX);
-//			Serial.println(stringOne);
-//	}
+    //check if the module is alive.
+	while(!isVM5GAAlive());
+	 while (Serial2.available()) {
+	   delay(10);  //small delay to allow input buffer to fill
+
+	   char c = Serial2.read();  //gets one byte from serial buffer
+
+	   readString += c;
+	 } //makes the string readString
+
+	 if (readString.length() >0) {
+	   Serial.println(readString); //prints string to serial port out
+
+	   readString=""; //clears variable for new input
+	 }
+}
+
+
+
+/*FUNCTION NAME:
+  * isVM5GAAlive
+  *
+  * DESCRIPTION:
+  * This checks whether VM5GA is alive.
+  *
+  *
+  * PARAMETERS:
+  * None.
+  *
+  * RETURN VALUES:
+  * 1  - if alive
+  * 0  - if not alive
+  */
+
+int isVM5GAAlive(){
+
+
 }
